9b715d352fc6af72284f96b31a057f0943777242
Khalilah <khalisahkhalilah1308@gmail.com>
2022-11-02 16:29:33 +0800

    first commit

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class UserController extends Controller
+{
+    function home(){
+
+        if(Auth::user()->user_role == 1){
+            return redirect()->route('dashboard_admin');
+        }else{
+            return view('dashboard');
+        }
+        return view('admin.dashboard');
+    }
+}
